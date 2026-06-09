9e559b767bfb5a46ebf98528176ee114f0efed28
Gaurav Tomar <gauravtomar977@gmail.com>
2026-06-10 01:06:46 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Class1.cpp
@@ -0,0 +1,39 @@
+#include<iostream>  
+using namespace std;    
+class Student
+{
+    public:
+    string name;
+    int age;
+    int roll_no;
+    Student(string name , int age , int roll_no){
+        this->name=name;
+        this->age=age;
+        this->roll_no=roll_no;
+        cout<<"Constructor called"<<endl;
+        
+    }
+};
+int main(){
+    Student s1("John",20,101);
+    cout<<"Name: "<<s1.name<<endl;
+        cout<<"Age: "<<s1.age<<endl;       
+        cout<<"Roll No: "<<s1.roll_no<<endl;
+    // Student *s1=new Student;
+    // s1.name="John";
+    // s1.age=20;
+    // s1.roll_no=101; 
+    // cout<<"Name: "<<s1.name<<endl;
+    // cout<<"Age: "<<s1.age<<endl;
+    // cout<<"Roll No: "<<s1.roll_no<<endl;   
+    // Student obj;
+    // cout<< sizeof(obj)<<endl; 
+
+    // (*s1).name="John";
+    // (*s1).age=20;
+    // (*s1).roll_no=101;
+    // cout<<"Name: "<<s1->name<<endl;
+    // cout<<"Age: "<<s1->age<<endl;
+    // cout<<"Roll No: "<<s1->roll_no<<endl;
+    
+}
---
+++ Class2.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+class Customer{
+   
+    string name;
+    int account_no;
+    int balance;
+
+    public:
+    // default constructor
+    Customer(){
+        name="Gaurav Tomar";
+        account_no=123456789;
+        balance=10000;
+    }
+
+    // parameterized constructor
+    Customer(string name , int account_no , int balance){
+        this->name=name;
+        this->account_no=account_no;
+        this->balance=balance;
+    }
+
+    // overloaded constructor
+    Customer(string name , int account_no){
+        this->name=name;
+        this->account_no=account_no;
+        this->balance=50;
+    }
+    // copy constructor
+    Customer(Customer &c){
+        name=c.name;
+        account_no=c.account_no;
+        balance=c.balance;
+    }
+    // inline constructor
+    // inline Customer(string n,int a , int b) : name(n), account_no(a), balance(b) {
+    //     cout<<"Inline constructor called"<<endl;
+    // }
+
+    void display(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Account No: "<<account_no<<endl;
+        cout<<"Balance: "<<balance<<endl;
+        cout<<"-----------------------------"<<endl;    
+    }
+};
+
+int main(){
+  Customer A1;  // default constructor
+  Customer A2("Sourav", 123, 20000); // parameterized constructor
+  Customer A3("vansh", 456); // overloaded constructor
+  Customer A4(A3); // copy constructor
+  Customer A5("Rudra", 789, 30000); // inline constructor
+  A1.display();
+  A2.display();
+  A3.display();
+  A4.display();
+  A5.display();
+}
